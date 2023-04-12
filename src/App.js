4ae93d59bb4e53d7b76f640755833ca0761ea2af
andmill <andrew.miller348@gmail.com>
2023-04-12 20:00:57 +0100

    Added additional headings to app home page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="App-title">Test Management Systems</h1>
+        <h2 className="App-description">Demo Website for GitHub Actions</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
