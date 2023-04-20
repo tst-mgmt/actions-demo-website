bdd764e8d932b820251318e0c42f2755c5d3d01e
andmill <andrew.miller348@gmail.com>
2023-04-20 22:08:25 +0100

    small update to site content for testing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">Test Management Systems</h1>
-        <h2 className="App-description">Demo Website for GitHub Actions</h2>
+        <h2 className="App-description">Demo Website for GitHub Actions Training</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
